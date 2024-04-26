7923def44c4f8f51fc1d7a19b3d8379a780d9fa4
zeusvs07 <zeusvs07@gmail.com>
2024-04-26 03:25:50 -0400

    menu principal

---
+++ Assets/Scripts/MenuPrincipal.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuPrincipal : MonoBehaviour
+{
+    public GameObject menuPrincipal;
+    public GameObject menuOpciones;
+    public void jugar ()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void salir() 
+    {
+        Debug.Log("salir...");
+        Application.Quit();
+    }       
+    
+    public void opciones()
+    {
+        menuPrincipal.SetActive(false);
+        menuOpciones.SetActive(true);
+    }
+
+    public void volver()
+    {
+        menuOpciones.SetActive(false );
+        menuPrincipal.SetActive(true);
+    }
+}
